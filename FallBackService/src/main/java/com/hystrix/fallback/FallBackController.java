0ec7efd99047273415cccff9df56eb8dcc69b323
Mathen <mathenkumaar.pk@gmail.com>
2021-04-12 23:35:06 +0530

    upd

--- FallBackService/src/main/java/com/hystrix/fallback/FallBackController.java
+++ FallBackService/src/main/java/com/hystrix/fallback/FallBackController.java
@@ -1,9 +1,12 @@
 package com.hystrix.fallback;
 
-import org.aspectj.lang.annotation.RequiredTypes;
-import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/fallback")
 public class FallBackController {
 
+	
+	
 }
